fa3bf68b3489d59f314993b8fa79dc8e93d3d836
alishahussain <alishahussain1229@gmail.com>
2024-02-26 22:51:35 -0800

    final alisha

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,8 +1,8 @@
 export var uri;
 if (location.hostname === "localhost") {
-        uri = "http://localhost:8086";
+        uri = "http://localhost:8181";
 } else if (location.hostname === "127.0.0.1") {
-        uri = "http://127.0.0.1:8086";
+        uri = "http://127.0.0.1:8181";
 } else {
         uri = "https://flask2.nighthawkcodingsociety.com";
 }
